fc78e9da99a093ce590b50aabf99e95958e5dc55
VigneshNallasivam <vickyshanestark@gmail.com>
2023-01-25 14:36:25 +0530

    Added : Validation of Email Using HTML Form

--- Sample.js
+++ Sample.js
@@ -10,3 +10,16 @@ function firstName() {
         alert("Validated Successfully..!!");
     }
 }
+
+function email() {
+    let pattern = new RegExp('[a-z]*[.][a-z]+[@][a-z]*[.][a-z]*[.][a-z]+');
+    let input = document.getElementById("email").value;
+    let status = pattern.test(input);
+    if (status==false) {
+        alert("Invalid email,Check Your User Mail-ID");
+    }
+    else
+    {
+        alert("Validated Successfully..!!");
+    }
+}
